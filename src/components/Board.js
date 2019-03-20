79f1bb092c4fc5c024d5240ad725015aedf249a1
josepplloo <josepplloo@gmail.com>
2019-03-20 16:48:18 -0500

    Board use two loops to make the squares instead of hardcoding them.

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,32 +3,28 @@ import Square from './Square.js';
 
 export default class Board extends React.Component {
 
-renderSquare(index) {
-  return <Square 
-    value = {this.props.squares[index]} 
-    onClick = {() => this.props.onClick(index)}
-  />;
-}
+  renderSquare(index) {
+    return <Square value={this.props.squares[index]} onClick={() => this.props.onClick(index)} />;
+  }
 
-render() {
-  return (
-    <div>
-      <div className="board-row">
-        {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
+
+  render() {
+    let square = -1;
+    return (
+      <div>
+      {
+        [1,2,3].map( () => {
+				  return <div className="board-row">
+            {
+            	[1,2,3].map( () => {
+                square ++;
+          			return (this.renderSquare(square))
+          		})
+            }
+          </div>
+      	})
+      }
       </div>
-      <div className="board-row">
-        {this.renderSquare(3)}
-        {this.renderSquare(4)}
-        {this.renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {this.renderSquare(6)}
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
-      </div>
-    </div>
-  );
-}
+    );
+  }
 }
