6631e8b2248843b17d041d147920e362dd5d2b9d
josepplloo <josepplloo@gmail.com>
2019-03-19 20:07:18 -0500

    Code Splitting

---
+++ src/components/Square.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Square (props) {
+  
+    return (
+    <button class="square" onClick={ props.onClick } >
+      {props.value}
+    </button>);
+}
